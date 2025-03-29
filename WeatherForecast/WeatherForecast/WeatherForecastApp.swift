e8048611cce45d1495d6ac06e7122dc0056b9508
dongeunshin <7017410@naver.com>
2025-03-29 15:52:39 +0900

    Views, structures, and properties section 완료

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by dong eun shin on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
